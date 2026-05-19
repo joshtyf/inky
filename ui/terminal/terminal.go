30c51037597e256531f55cedaec1d81b5bb78469
Joshua Tan <tan.yin.feng@hotmail.com>
2026-05-19 23:24:49 +0800

    fix: line rendering bug when file is bigger than screen

--- ui/terminal/terminal.go
+++ ui/terminal/terminal.go
@@ -59,7 +59,7 @@ func NewTerminal() *Terminal {
 		topLine:                 0,
 		currentLine:             0,
 		screenHeight:            h,
-		renderCache:             make([]string, h),
+		renderCache:             make([]string, h-1),
 		markdownViewCurrentLine: 0,
 		lastContentVersion:      -1,
 	}
@@ -174,15 +174,15 @@ func (t *Terminal) Update(es *core.EditorState) error {
 	}
 	var targetLine, targetCol int
 	if es.ViewMode == core.MarkdownView {
-		targetLine = t.markdownViewCurrentLine + 1
+		targetLine = t.markdownViewCurrentLine
 		targetCol = 1
 	} else {
-		targetLine = es.CursorCurrentLine + 1
+		targetLine = es.CursorCurrentLine
 		targetCol = es.CursorCurrentCol + 1
 	}
 	// Update topLine for scrolling before rendering
-	if targetLine >= t.topLine+t.screenHeight {
-		t.topLine = targetLine - t.screenHeight + 1
+	if targetLine >= t.topLine+t.screenHeight-1 {
+		t.topLine = targetLine - t.screenHeight + 2
 	} else if targetLine < t.topLine {
 		t.topLine = targetLine
 	}
@@ -191,14 +191,14 @@ func (t *Terminal) Update(es *core.EditorState) error {
 	t.renderText()
 	t.renderUI(es)
 	// Reposition cursor explicitly after all rendering
-	fmt.Print(AnsiMoveCursor(targetLine-t.topLine, targetCol))
+	fmt.Print(AnsiMoveCursor(targetLine-t.topLine+1, targetCol))
 	fmt.Print(AnsiCursorShow)
 	return nil
 }
 
 func (t *Terminal) updateTextCache(es *core.EditorState) {
 	renderer := renderers[es.ViewMode]
-	for i := 0; i < t.screenHeight; i++ {
+	for i := 0; i < t.screenHeight-1; i++ {
 		lineNum := t.topLine + i
 		line, err := renderer.Render(lineNum, es)
 		if err != nil {
