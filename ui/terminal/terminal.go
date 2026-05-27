62004bc8c8c32855dec8978dd0489996aadf3575
Joshua Tan <tan.yin.feng@hotmail.com>
2026-05-27 17:05:28 +0800

    refactor: processStateAndRender

--- ui/terminal/terminal.go
+++ ui/terminal/terminal.go
@@ -215,43 +215,7 @@ func (t *Terminal) runRenderLoop(ctx context.Context) chan<- struct{} {
 				return
 			case <-signalCh:
 				es := t.lastEditorState
-				if es.Version != t.lastContentVersion {
-					t.markdownViewCurrentLine = 0
-					t.lastContentVersion = es.Version
-				}
-				if es.ViewMode == core.MarkdownView && es.LastKeyPresssed != nil {
-					switch es.LastKeyPresssed.Code {
-					case core.ArrowDown:
-						if t.markdownViewCurrentLine < renderers[core.MarkdownView].LastLine(es) {
-							t.markdownViewCurrentLine++
-						}
-					case core.ArrowUp:
-						if t.markdownViewCurrentLine > 0 {
-							t.markdownViewCurrentLine--
-						}
-					}
-				}
-				var targetLine, targetCol int
-				if es.ViewMode == core.MarkdownView {
-					targetLine = t.markdownViewCurrentLine
-					targetCol = 1
-				} else {
-					targetLine = es.CursorCurrentLine
-					targetCol = es.CursorCurrentCol + 1
-				}
-				// Update topLine for scrolling before rendering
-				if targetLine >= t.topLine+t.screenHeight-1 {
-					t.topLine = targetLine - t.screenHeight + 2
-				} else if targetLine < t.topLine {
-					t.topLine = targetLine
-				}
-				fmt.Print(AnsiCursorHide)
-				t.updateTextCache(es)
-				t.renderText()
-				t.renderUI(es)
-				// Reposition cursor explicitly after all rendering
-				fmt.Print(AnsiMoveCursor(targetLine-t.topLine+1, targetCol))
-				fmt.Print(AnsiCursorShow)
+				t.processStateAndRender(es)
 			}
 		}
 	}()
@@ -285,6 +249,46 @@ func (t *Terminal) listenForResize(ctx context.Context) {
 			}
 		}
 	}()
+}
+
+func (t *Terminal) processStateAndRender(es *core.EditorState) {
+	if es.Version != t.lastContentVersion {
+		t.markdownViewCurrentLine = 0
+		t.lastContentVersion = es.Version
+	}
+	if es.ViewMode == core.MarkdownView && es.LastKeyPresssed != nil {
+		switch es.LastKeyPresssed.Code {
+		case core.ArrowDown:
+			if t.markdownViewCurrentLine < renderers[core.MarkdownView].LastLine(es) {
+				t.markdownViewCurrentLine++
+			}
+		case core.ArrowUp:
+			if t.markdownViewCurrentLine > 0 {
+				t.markdownViewCurrentLine--
+			}
+		}
+	}
+	var targetLine, targetCol int
+	if es.ViewMode == core.MarkdownView {
+		targetLine = t.markdownViewCurrentLine
+		targetCol = 1
+	} else {
+		targetLine = es.CursorCurrentLine
+		targetCol = es.CursorCurrentCol + 1
+	}
+	// Update topLine for scrolling before rendering
+	if targetLine >= t.topLine+t.screenHeight-1 {
+		t.topLine = targetLine - t.screenHeight + 2
+	} else if targetLine < t.topLine {
+		t.topLine = targetLine
+	}
+	fmt.Print(AnsiCursorHide)
+	t.updateTextCache(es)
+	t.renderText()
+	t.renderUI(es)
+	// Reposition cursor explicitly after all rendering
+	fmt.Print(AnsiMoveCursor(targetLine-t.topLine+1, targetCol))
+	fmt.Print(AnsiCursorShow)
 }
 
 func (t *Terminal) updateTextCache(es *core.EditorState) {
